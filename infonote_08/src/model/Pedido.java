5e60806c28061044f4f2b066f422e2b146018005
Lucas Soares <lucassoaresassis@hotmail.com>
2018-12-05 21:18:59 -0200

    infonote_08

---
+++ infonote_08/src/model/Pedido.java
@@ -0,0 +1,111 @@
+package model;
+
+public class Pedido {
+
+	private String dataEmissao;
+	private String formaDePagamento;
+	private String situacao;
+	private double valorTotal;
+	private String numeroPedido;
+
+	private Endereco enderecoEntrega;
+	private ItemDePedido itens[] = new ItemDePedido[10];
+
+	public String getDataEmissao() {
+		return dataEmissao;
+	}
+
+	public void setDataEmissao(String dataEmissao) {
+		this.dataEmissao = dataEmissao;
+	}
+
+	public String getFormaDePagamento() {
+		return formaDePagamento;
+	}
+
+	public void setFormaDePagamento(String formaDePagamento) {
+		this.formaDePagamento = formaDePagamento;
+	}
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+	public String getNumeroPedido() {
+		return numeroPedido;
+	}
+
+	public void setNumeroPedido(String numeroPedido) {
+		this.numeroPedido = numeroPedido;
+	}
+
+	public double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public Endereco getEnderecoEntrega() {
+		return enderecoEntrega;
+	}
+
+	public void setEnderecoEntrega(Endereco enderecoEntrega) {
+		this.enderecoEntrega = enderecoEntrega;
+	}
+	
+	public ItemDePedido[] getItens() {
+		return itens;
+	}
+
+	public void setItens(ItemDePedido[] itens) {
+		this.itens = itens;
+	}
+
+	public Pedido() {
+		super();
+	}
+
+	public Pedido(String numeroPedido, String dataEmissao, String formaDePagamento, String situacao, int numero,
+			double valorTotal, Endereco enderecoEntrega) {
+		super();
+		this.numeroPedido = numeroPedido;
+		this.valorTotal = valorTotal;
+		this.dataEmissao = dataEmissao;
+		this.formaDePagamento = formaDePagamento;
+		this.situacao = situacao;
+		this.enderecoEntrega = enderecoEntrega;
+	}
+
+	public String toString(){
+		final String ENTER = "\n";
+		String retValue = "";
+		retValue = "Informações sobre Pedido:" + ENTER +
+		"Número do Pedido: " + numeroPedido + ENTER +
+		"Data de Emissão: " + dataEmissao + ENTER +
+		"Forma de pagamento: " + formaDePagamento + ENTER +
+		"Valor Total: " + valorTotal + ENTER +
+		"Situação: " + situacao + ENTER +
+		"Endereço de Entrega: " + ENTER +
+		"Itens: ";
+		for (int i = 0; i < itens.length; i++){
+		retValue += itens[i] + ENTER;
+		}
+		return retValue;
+		}
+	
+	public boolean inserirItem(ItemDePedido item) {
+		for (int i = 0; i < itens.length; i++) {
+			if (itens[i] == null) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+}
